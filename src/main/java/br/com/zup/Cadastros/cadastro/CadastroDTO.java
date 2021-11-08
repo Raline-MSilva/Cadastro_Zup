86c47b300e5a50f91970f2df0e27e3895d5f80a8
Raline Silva <ralinesilva1@gmail.com>
2021-11-08 19:30:11 -0300

    Alterar cadastroDTO

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroDTO.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroDTO.java
@@ -12,7 +12,6 @@ public class CadastroDTO {
     private boolean moraSozinho;
     private boolean temPet;
     private int idade;
-    private LocalDate dataDoCadastro;
 
     public CadastroDTO() {
     }
@@ -87,14 +86,6 @@ public class CadastroDTO {
 
     public void setIdade(int idade) {
         this.idade = idade;
-    }
-
-    public LocalDate getDataDoCadastro() {
-        return dataDoCadastro;
-    }
-
-    public void setDataDoCadastro(LocalDate dataDoCadastro) {
-        this.dataDoCadastro = dataDoCadastro;
     }
 
 }
