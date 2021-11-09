0542536abdc41efac393a956913e323e55df159d
Raline Silva <ralinesilva1@gmail.com>
2021-11-08 21:00:28 -0300

    Implementar lista com os cadastros

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
@@ -2,5 +2,8 @@ package br.com.zup.Cadastros.cadastro;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CadastroRepository extends CrudRepository<Cadastro, String> {
+    List<Cadastro> findAll();
 }
