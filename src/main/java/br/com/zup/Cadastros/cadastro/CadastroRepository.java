a67b9685eae34e5456676f07cb1e017c8aca019d
Raline Silva <ralinesilva1@gmail.com>
2021-11-08 21:05:14 -0300

    Filtar cadastros mora sozinho

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface CadastroRepository extends CrudRepository<Cadastro, String> {
     List<Cadastro> findAll();
+    List<Cadastro> findAllByMoraSozinho(boolean moraSozinho);
 }
